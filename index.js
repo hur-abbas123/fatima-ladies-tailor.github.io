196c811bcb5eca487279852eee6f3e1d32d260c9
hur-abbas123 <80173986+hur-abbas123@users.noreply.github.com>
2021-03-07 00:50:54 +0500

    fatima ladies tailor

---
+++ index.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar-menu');
+const navLogo = document.querySelector('#navbar-logo');
+
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
+
+
+$(function(){
+  $(".menu-image").click(function(){
+ $(".navbar-menu").slideToggle();
+  })
+});
